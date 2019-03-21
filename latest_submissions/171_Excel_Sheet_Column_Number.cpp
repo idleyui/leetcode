a9a55f2ce6604cb0340f1f3f9868cd93ec05b601
lyd <leelddd@outlook.com>
2019-03-21 22:04:27 +0800

    3/21 change structure

--- latest_submissions/171_Excel_Sheet_Column_Number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int titleToNumber(string s) {
-        int num = 0;
-        for(auto &c: s)
-            num = num * 26 + (c - 'A' + 1);
-        return num;
-    }
-};
