29aff951bcaf0ac4547e963f0196416946509cb2
lyd <leelddd@outlook.com>
2019-03-22 13:40:09 +0800

    3/22 last three to go!

--- solution/234_Palindrome_Linked_List.cpp
+++ solution/234_Palindrome_Linked_List.cpp
@@ -1,7 +1,23 @@
 #include "alg.h"
 
-bool isPalindrome(ListNode *head) {
+ListNode *new_head;
 
+bool equal(ListNode *head) {
+    if (!head) return true;
+    bool is_pal = equal(head->next) && (new_head->val == head->val);
+    new_head = new_head->next;
+    return is_pal;
+}
+
+bool isPalindrome(ListNode *head) {
+    new_head = head;
+    return equal(head);
+}
+
+// find mid -> reverse right -> left == right?
+bool isPalindrome2(ListNode *head) {
+//    todo
+    return true;
 }
 
 int main() {
