a8cc48d345f84b768d78969b49a6a6a0428b8a37
lyd <leelddd@outlook.com>
2019-04-07 11:49:12 +0800

    4/6

--- solution/402_Remove_K_Digits.cpp
+++ solution/402_Remove_K_Digits.cpp
@@ -1,34 +1,22 @@
 #include "alg.h"
 
+//https://leetcode.com/problems/remove-k-digits/discuss/88743/C%2B%2B-6ms-10-lines-solution-with-comments
 string removeKdigits(string num, int k) {
-    string s;
-    int i = 0;
-    for (; i < num.size(); i++) {
-        char c = num[i];
-        if (k == 0) {
-            break;
-        }
-        if (c == '0' && s.empty()) { if (s.empty())continue; }
-        else if (c == '0' && s.size() == 1) {
-            s.pop_back();
+    string ans;
+
+    for (char c: num) {
+        while (ans.size() && ans.back() > c && k) {
+            ans.pop_back();
             k--;
-        } else if (s.empty()) {
-            s.push_back(c);
-        } else {
-            char top = s.back();
-            while (!s.empty() && c <= top) {
-                k--;
-                s.pop_back();
-            }
-            s.push_back(c);
         }
+        if (ans.size() || c != '0') { ans.push_back(c); }
     }
-    string rt = (k < s.size() ? s.substr(k) : "") + num.substr(i);
-    return rt.size() > 0 ? rt : "0";
+    while (ans.size() && k--) { ans.pop_back(); }
+    return ans.empty() ? "0" : ans;
 }
 
 int main() {
-    cout << removeKdigits("1432219", 3) << endl;
+//    cout << removeKdigits("1432219", 3) << endl;
     cout << removeKdigits("10200", 1) << endl;
     cout << removeKdigits("10", 2) << endl;
     cout << removeKdigits("10201", 2) << endl;
