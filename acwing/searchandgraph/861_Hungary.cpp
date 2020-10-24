3e076c11a5e9d362824603e3972d9c0d19bcb86f
lyd <leelddd@outlook.com>
2020-10-24 19:38:40 +0800

    1024 hungary

---
+++ acwing/searchandgraph/861_Hungary.cpp
@@ -0,0 +1,52 @@
+// https://www.acwing.com/problem/content/863/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+
+const int N = 510, M = 100010;
+
+int n1, n2, m;
+int h[N], e[M], ne[M], idx;
+int st[N], match[N];
+
+int add(int a, int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+bool find(int x) {
+    for (int i = h[x]; i != -1; i = ne[i]) {
+        int j = e[i];
+        if (!st[j]) {
+            st[j] = true;
+            if (match[j] == 0 || find(match[j])) {
+                match[j] = x;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+int main() {
+    cin >> n1 >> n2 >> m;
+    memset(h, -1, sizeof h);
+
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        cin >> a >> b;
+        add(a, b);
+    }
+
+    int res = 0;
+    for (int i = 1; i <= n1; ++i) {
+        memset(st, false, sizeof st);
+        if (find(i)) res++;
+    }
+
+    cout << res;
+}
+
