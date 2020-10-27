10e433e02849ba0ce9263c6f4c3144c465ff71fd
lyd <leelddd@outlook.com>
2020-10-27 19:30:17 +0800

    10/27 some lc

--- solution/0166_Fraction_to_Recurring_Decimal.cpp
+++ solution/0166_Fraction_to_Recurring_Decimal.cpp
@@ -1,7 +1,35 @@
 #include "alg.h"
 
 string fractionToDecimal(int numerator, int denominator) {
-    
+    if (numerator == 0) return "0";
+    string result;
+
+    if ((numerator > 0) ^ (denominator > 0)) result += '-';
+
+    long long num = (long long) numerator;
+    long long denom = (long long) denominator;
+    num = llabs(num);
+    denom = llabs(denom);
+    result.append(to_string(num / denom));
+
+    num %= denom;
+    if (num == 0) return result;
+
+    result.push_back('.');
+    int idx = result.size() - 1;
+    unordered_map<int, int> record;
+    while (num && !record.count(num)) {
+        record[num] = ++idx;
+        num *= 10;
+        result += to_string(num / denom);
+        num %= denom;
+    }
+
+    if (record.count(num)) {
+        result.insert(record[num], "(");
+        result += ')';
+    }
+    return result;
 }
 
 int main() {
