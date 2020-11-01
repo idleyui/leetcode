3b629f2e8f4448df6eb7b8735384402be1dd5419
lyd <leelddd@outlook.com>
2020-11-01 14:33:13 +0800

    11/1 edit distance

--- acwing/dp/897_Longest_Common_Subsequence.cpp
+++ acwing/dp/897_Longest_Common_Subsequence.cpp
@@ -7,10 +7,13 @@ int n, m;
 int dp[N][N];
 string s1, s2;
 
+// 以 A[i], B[j] 是否包含在子序列中为划分，因此有四种情况
 int lcs() {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; ++j) {
+            // 01 00 10 00
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            // 11
             if (s1[i - 1] == s2[j - 1]) dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
         }
     }
